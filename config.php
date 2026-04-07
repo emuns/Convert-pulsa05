60b6af5bc6de4f3cb534c38c9b5525120b50bb6f
Emuna <hermantobonx1@gmail.com>
2026-04-07 18:11:56 +0700

    fix config railway

--- config.php
+++ config.php
@@ -1,16 +1,13 @@
 <?php
-// AUTO DETECT (LOCAL / RAILWAY)
-$host = getenv("MYSQLHOST") ?: "localhost";
-$user = getenv("MYSQLUSER") ?: "root";
-$pass = getenv("MYSQLPASSWORD") ?: "";
-$db   = getenv("MYSQLDATABASE") ?: "convert_pulsa";
-$port = getenv("MYSQLPORT") ?: 3306;
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$pass = getenv("MYSQLPASSWORD");
+$db   = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
 
-$conn = new mysqli($host,$user,$pass,$db,$port);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
-if($conn->connect_error){
- die("DB Error: ".$conn->connect_error);
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
 }
-
-session_start();
 ?>
